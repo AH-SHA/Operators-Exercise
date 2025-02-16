cb82fe8aa2113ba35f6886684b0d75591316cbbd
Shannon Ashford <email.sa.coder@gmail.com>
2025-02-15 23:19:31 -0500

    Checked solution

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            int a = 17;
+            int b = 4;
+        
+                var sum = a + b; // Output is 21
+                var subtract = a - b; // Output is 13
+                var multiply = a * b;// Output is 61
+                var quotient = a / b;// Output is 4
+                var remainder = a % b;// Output is 1
+
+            Console.WriteLine($"{a}+{b} is {sum}.");
+            Console.WriteLine($"{a}-{b} is {subtract}.");
+            Console.WriteLine($"{a}*{b} is {multiply}.");
+            Console.WriteLine($"{a}/{4} is {quotient} remainder {remainder}.");
+           
+
         }
+        
     }
 }
